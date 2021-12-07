773854e54f9683918a86c393349d9acfb328d626
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-07 09:48:06 +0300

    go to another activity

--- app/src/main/java/com/example/uiexample3/MainActivity.java
+++ app/src/main/java/com/example/uiexample3/MainActivity.java
@@ -3,20 +3,55 @@ package com.example.uiexample3;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.lang.annotation.Native;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RelativeLayout taskBtn;
+    private RelativeLayout reminderBtn;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.taskBtn:
+                goToTasksActivity();
+                break;
+            case R.id.reminderBtn:
+                goToRemindersActivity();
+                break;
+        }
+    }
+
+    public void goToTasksActivity(){
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToRemindersActivity(){
+        Intent intent = new Intent(this, RemindersActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        taskBtn = findViewById(R.id.taskBtn);
+        taskBtn.setOnClickListener(this);
+
+        reminderBtn= findViewById(R.id.reminderBtn);
+        reminderBtn.setOnClickListener(this);
+
     }
 
 //    @Override
