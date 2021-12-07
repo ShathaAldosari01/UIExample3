48d6ebdc7703b503ab02216bde8d5e441eb6e21c
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-07 11:30:40 +0300

    go to another activity

--- app/src/main/java/com/example/uiexample3/RemindersActivity.java
+++ app/src/main/java/com/example/uiexample3/RemindersActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class RemindersActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        fab = findViewById(R.id.reminderAddBtn);
     }
 }
