3756f2d190005a1c8cec038e38816456be630f65
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-07 04:42:52 +0300

    - added colors in xml color - change the color in them.xml - added horizontal activity - added settengs then make it as comment since i don't need it

--- app/src/main/java/com/example/uiexample3/MainActivity.java
+++ app/src/main/java/com/example/uiexample3/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.uiexample3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.lang.annotation.Native;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.main_menu, menu);
+//        return true;
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+//        switch (item.getItemId()){
+//            case R.id.settings_menu:
+//                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onContextItemSelected(item);
+//        }
+//
+//    }
+
 }
