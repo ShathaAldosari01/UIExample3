07f9254c83fb75591ded11529cbe18110245a062
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-07 11:49:02 +0300

    added form activity

--- app/src/main/java/com/example/uiexample3/RemindersActivity.java
+++ app/src/main/java/com/example/uiexample3/RemindersActivity.java
@@ -2,7 +2,9 @@ package com.example.uiexample3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,5 +17,12 @@ public class RemindersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reminders);
 
         fab = findViewById(R.id.reminderAddBtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RemindersActivity.this, ReminderFormActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/uiexample3/TasksActivity.java
+++ app/src/main/java/com/example/uiexample3/TasksActivity.java
@@ -2,13 +2,27 @@ package com.example.uiexample3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TasksActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        fab = findViewById(R.id.taskAddBtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TasksActivity.this, TaskFormActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
