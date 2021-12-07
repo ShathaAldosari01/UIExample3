cda4bd8c814a8a89acd94295936247f1cb20d6e5
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-07 13:14:50 +0300

    nothing much

--- app/src/main/java/com/example/uiexample3/ReminderFormActivity.java
+++ app/src/main/java/com/example/uiexample3/ReminderFormActivity.java
@@ -2,13 +2,27 @@ package com.example.uiexample3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ReminderFormActivity extends AppCompatActivity {
 
+    private Button addReminder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_form);
+        addReminder = findViewById(R.id.addReminder);
+        addReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ReminderFormActivity.this, "reminder was added", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ReminderFormActivity.this, RemindersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/uiexample3/TaskFormActivity.java
+++ app/src/main/java/com/example/uiexample3/TaskFormActivity.java
@@ -2,13 +2,28 @@ package com.example.uiexample3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TaskFormActivity extends AppCompatActivity {
 
+    private Button addTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_form);
+
+        addTask= findViewById(R.id.addTask);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TaskFormActivity.this, "task has been added", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(TaskFormActivity.this, TasksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
